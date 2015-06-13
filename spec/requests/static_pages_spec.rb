cda7d70ecf6ecc16e56f8aa108e3c78ebf4178f0
dorachan1029 <h.maruyama1029@gmail.com>
2015-06-13 19:14:36 +0900

    [add_3_2]テストが通るようにコードを修正

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App" do
       visit '/static_pages/home'
-      expect(page).to habe_content('Sample App')
+      expect(page).to have_content('Sample App')
     end
   end
 end
